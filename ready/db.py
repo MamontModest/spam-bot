ea56a1d5fee960785ca1f955db6752a5823e9554
YourUserName <YourEmailAddress>
2023-01-17 22:48:31 +0300

    8 clients

--- ready/db.py
+++ ready/db.py
@@ -1,17 +1,6 @@
 import sqlite3
 con = sqlite3.connect("tutorial.db")
-cur = con.cursor()
-cur.execute("select * from spam")
-print(cur.fetchall())
-cur.execute("select * from status")
-print(cur.fetchall())
-cur.execute("select * from stat")
-print(cur.fetchall())
-cur.execute("select * from pars")
-print(cur.fetchall())
-cur.execute("select * from line")
-print(cur.fetchall())
-
+cur=con.cursor()
 cur.execute("create table spam(id_user,message,tg_kanal)")
 cur.execute("Create table status(id,status)")
 cur.execute("Create table stat(status,id_creator,tg_kanal)")
